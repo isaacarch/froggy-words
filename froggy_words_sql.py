8532f78e9594a84ce33b9aedfcc30cbb39a93f92
Isaac C <isaacchris112@gmail.com>
2026-05-10 12:14:50 +0100

    leaderboard working, and cleans punctuation!

---
+++ froggy_words_sql.py
@@ -0,0 +1,66 @@
+import sqlite3
+
+DATA_BASE = "froggy_board.db"
+
+def create_table():
+    MAKE_TABLE = '''CREATE TABLE IF NOT EXISTS Leaderboard(
+    User_ID INTEGER PRIMARY KEY,
+    Wins INTEGER);'''
+
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        conn.execute(MAKE_TABLE)
+        db.commit()
+
+def reset_leaderboard():
+    TRUNCATE_TABLE = '''DELETE FROM Leaderboard;'''
+
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        conn.execute(TRUNCATE_TABLE)
+        db.commit()
+
+def add_new_member(uid):
+    ADD_USER = f'''INSERT INTO Leaderboard VALUES ({uid}, 1);'''
+
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        conn.execute(ADD_USER)
+        db.commit()
+
+def update_score(uid, score):
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        UPDATE_SCORE = f'''UPDATE Leaderboard SET Wins = {score} WHERE User_ID = {uid};'''
+        conn.execute(UPDATE_SCORE)
+        db.commit()
+
+def add_win(uid):
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        FIND_ROW = f'''SELECT Wins FROM Leaderboard WHERE User_ID = {uid};'''
+        conn.execute(FIND_ROW)
+        row = conn.fetchone()
+    if not row:
+        add_new_member(uid)
+    else:
+        new_score = row[0] + 1
+        update_score(uid, new_score)
+
+def get_leaderboard():
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        GET_LEADERBOARD = f'''SELECT * FROM Leaderboard;'''
+        conn.execute(GET_LEADERBOARD)
+        return conn.fetchall()
+
+def get_user_score(uid):
+    with sqlite3.connect(DATA_BASE) as db:
+        conn = db.cursor()
+        GET_SCORE = f'''SELECT Wins FROM Leaderboard WHERE User_ID={uid}'''
+        conn.execute(GET_SCORE)
+        row = conn.fetchone()
+        return row[0] if row else None
+
+
+create_table()
--- froggy_words_submit.py
+++ froggy_words_submit.py
@@ -1,10 +1,13 @@
 import discord
 from discord import app_commands
-
 import datetime as dt
+import string
+
+from froggy_words_sql import add_win, reset_leaderboard, get_leaderboard, get_user_score
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 MY_GUILD = discord.Object(id=1502751209247740014)
 
@@ -22,10 +25,14 @@ class MyClient(discord.Client):
 
 client = MyClient(intents=intents)
 
+DIRTY_WORD = None
 SECRET_WORD = None
 PREV_AUTHOR = None
 WORD_SETTER = None
 TIMEOUT = False
+
+def clean(s):
+    return ''.join(filter(lambda x: x not in string.punctuation and x not in string.whitespace, s)).lower()
 
 async def timeout(user):
     if TIMEOUT:
@@ -44,21 +51,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global PREV_AUTHOR, SECRET_WORD, WORD_SETTER
+    global PREV_AUTHOR, SECRET_WORD, WORD_SETTER, DIRTY_WORD
     if message.author == client.user or message.author == PREV_AUTHOR or message.author == WORD_SETTER or not SECRET_WORD:
         return
 
-    if SECRET_WORD == message.content.lower():
+    if SECRET_WORD == clean(message.content):
         await message.add_reaction("🐸")
+        add_win(message.author.id)
+        score = get_user_score(message.author.id)
         await message.channel.send(
-            f"{message.author.mention} has successfully guessed the secret word: {SECRET_WORD}!"
+            f"{message.author.mention} has successfully guessed the secret word: **{DIRTY_WORD}**! Their new score is {score}."
         )
         PREV_AUTHOR = None
         WORD_SETTER = None
         SECRET_WORD = None
+        DIRTY_WORD = None
         return
 
-    if SECRET_WORD in message.content.lower():
+    if SECRET_WORD in clean(message.content):
         await message.add_reaction("🐸")
         await timeout(message.author)
         PREV_AUTHOR = message.author
@@ -71,7 +81,7 @@ async def on_message(message):
 )
 async def set_secret_word(interaction: discord.Interaction, secret_word: str):
     """Choose a secret word for members to guess together!"""
-    global SECRET_WORD, WORD_SETTER, PREV_AUTHOR
+    global SECRET_WORD, WORD_SETTER, PREV_AUTHOR, DIRTY_WORD
 
     if SECRET_WORD:
         await interaction.response.send_message(
@@ -79,7 +89,8 @@ async def set_secret_word(interaction: discord.Interaction, secret_word: str):
             ephemeral=True
         )
     else:
-        SECRET_WORD = secret_word.lower()
+        SECRET_WORD = clean(secret_word)
+        DIRTY_WORD = secret_word
         WORD_SETTER = interaction.user
         PREV_AUTHOR = None
         await interaction.response.send_message(
@@ -89,10 +100,11 @@ async def set_secret_word(interaction: discord.Interaction, secret_word: str):
 @client.tree.command()
 async def clear_secret_word(interaction: discord.Interaction):
     """Clear the current secret word."""
-    global SECRET_WORD, WORD_SETTER, PREV_AUTHOR
+    global SECRET_WORD, WORD_SETTER, PREV_AUTHOR, DIRTY_WORD
     SECRET_WORD = None
     WORD_SETTER = None
     PREV_AUTHOR = None
+    DIRTY_WORD = None
     await interaction.response.send_message(
             f"{interaction.user.mention} has cleared the current secret word!"
     )
@@ -113,6 +125,28 @@ async def timeout_on_frog(interaction: discord.Interaction, enabled: bool):
                 f"{interaction.user.mention} has disabled timeouts on correct guesses! Users will be spared from the frog's wrath for now..."
         )
     print(f"TIMEOUT={TIMEOUT}")
+
+@client.tree.command()
+@app_commands.checks.has_permissions(moderate_members=True)
+async def wipe_leaderboard(interaction: discord.Interaction):
+    """DANGER: Clear the leaderboard for the game. This cannot be undone!!"""
+    reset_leaderboard()
+    await interaction.response.send_message(
+        f"{interaction.user.mention} has reset the leaderboard."
+    )
+
+@client.tree.command()
+async def show_leaderboard(interaction: discord.Interaction):
+    """Display the leaderboard for the game!"""
+    embed = discord.Embed(title="Froggy Words leaderboard:", color=discord.Colour.gold())
+    leaderboard = sorted(get_leaderboard(), key=lambda x: x[1], reverse=True)
+    for pos, (id, wins) in enumerate(leaderboard):
+        member = interaction.guild.get_member(id)
+        embed.add_field(name=f"{pos+1} - {member.display_name}", value=f"{wins} words guessed", inline=False)
+        if pos+1 > 9:
+            break
+    await interaction.response.send_message(embed=embed)
+
 
 
 # ## Role stuff non-functional, i think it would require a DB, so not doing it.
