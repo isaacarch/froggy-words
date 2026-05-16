f35bb8942c2892d765033fdb82887accb1fe22fc
Isaac C <isaacchris112@gmail.com>
2026-05-16 15:06:05 +0100

    parameterized SQL

--- froggy_words_sql.py
+++ froggy_words_sql.py
@@ -31,15 +31,17 @@ def add_new_member(uid):
 def update_score(uid, score):
     with sqlite3.connect(DATA_BASE) as db:
         conn = db.cursor()
-        UPDATE_SCORE = f'''UPDATE Leaderboard SET Wins = {score} WHERE User_ID = {uid};'''
-        conn.execute(UPDATE_SCORE)
+        UPDATE_SCORE = '''UPDATE Leaderboard SET Wins = ? WHERE User_ID = ?;'''
+        VALUES = (score, uid)
+        conn.execute(UPDATE_SCORE, VALUES)
         db.commit()
 
 def add_win(uid):
     with sqlite3.connect(DATA_BASE) as db:
         conn = db.cursor()
-        FIND_ROW = f'''SELECT Wins FROM Leaderboard WHERE User_ID = {uid};'''
-        conn.execute(FIND_ROW)
+        FIND_ROW = '''SELECT Wins FROM Leaderboard WHERE User_ID = ?;'''
+        VALUES = (uid,)
+        conn.execute(FIND_ROW, VALUES)
         row = conn.fetchone()
     if not row:
         add_new_member(uid)
@@ -50,15 +52,16 @@ def add_win(uid):
 def get_leaderboard():
     with sqlite3.connect(DATA_BASE) as db:
         conn = db.cursor()
-        GET_LEADERBOARD = f'''SELECT * FROM Leaderboard;'''
+        GET_LEADERBOARD = '''SELECT * FROM Leaderboard;'''
         conn.execute(GET_LEADERBOARD)
         return conn.fetchall()
 
 def get_user_score(uid):
     with sqlite3.connect(DATA_BASE) as db:
         conn = db.cursor()
-        GET_SCORE = f'''SELECT Wins FROM Leaderboard WHERE User_ID={uid}'''
-        conn.execute(GET_SCORE)
+        GET_SCORE = '''SELECT Wins FROM Leaderboard WHERE User_ID = ?'''
+        VALUES = (uid,)
+        conn.execute(GET_SCORE, VALUES)
         row = conn.fetchone()
         return row[0] if row else None
 
